9bb40954d95ebe1c280b702a2967d2165d15e635
Nehul Krushna Gangadhar <107269633+Nehul-Krushna@users.noreply.github.com>
2023-08-06 11:40:18 +0530

    V1

---
+++ public/client.js
@@ -0,0 +1,57 @@
+const socket = io()
+
+var name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message_area')
+do{
+    name=prompt('Please enter your name:')
+}while(!name)
+textarea.addEventListener('keyup',(e) =>{
+    if(e.key ==='Enter'){
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message){
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+
+    //apend 
+    appendMessage(msg,'outgoing')
+    textarea.value=""
+    scrollToBottom()
+    //send msg to server
+socket.emit('message',msg)
+}
+
+
+function appendMessage(msg,type){
+    let mainDiv= document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className,'message')
+    var date=new Date()
+    var hours=date.getHours()
+    var min=date.getMinutes()
+    var time=hours+":"+min
+    let markup =`
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+        <h6>${time}</h6>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+}
+
+
+//Recieve the message
+socket.on('message',(msg) =>{
+    appendMessage(msg,'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom()
+{
+    messageArea.scrollTop=messageArea.scrollHeight
+}
